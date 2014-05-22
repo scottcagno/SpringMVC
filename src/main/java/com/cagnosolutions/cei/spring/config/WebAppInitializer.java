8f2307c2008c69d42d8254826ce57b0b70e40213
scottcagno <scottiecagno@gmail.com>
2014-05-22 16:18:22 -0400

    first

--- src/main/java/com/cagnosolutions/cei/spring/config/WebAppInitializer.java
+++ src/main/java/com/cagnosolutions/cei/spring/config/WebAppInitializer.java
@@ -1,18 +1,18 @@
-package com.cagnosolutions.cei.spring.config;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] {RootConfig.class};
-	}
-	
-	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[] {WebConfig.class};
-	}
-
-	protected String[] getServletMappings() {
-		return new String[] {"/"};
-	}
-}
+//package com.cagnosolutions.cei.spring.config;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//	protected Class<?>[] getRootConfigClasses() {
+//		return new Class<?>[] {RootConfig.class};
+//	}
+//	
+//	protected Class<?>[] getServletConfigClasses() {
+//		return new Class<?>[] {WebConfig.class};
+//	}
+//
+//	protected String[] getServletMappings() {
+//		return new String[] {"/"};
+//	}
+//}
---
+++ src/main/java/com/cagnosolutions/cei/spring/domain/User.java
@@ -0,0 +1,54 @@
+package com.cagnosolutions.cei.spring.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String email;
+	
+	// JPA
+	protected User() {}
+	
+	public User(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+	
+	public String toString() {
+		return String.format("User #%d, %s, %s", id, name, email);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}

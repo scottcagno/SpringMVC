4c41297fed3071e578fa618cbc7526408efdeaa3
gregpechiro <gregpechiro@yahoo.com>
2014-05-23 14:48:22 -0400

    update

--- src/main/java/com/cagnosolutions/cei/spring/controller/SessionController.java
+++ src/main/java/com/cagnosolutions/cei/spring/controller/SessionController.java
@@ -5,20 +5,18 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cagnosolutions.cei.spring.domain.User;
 import com.cagnosolutions.cei.spring.service.UserService;
 
 @Controller
 @Scope("request")
-@RequestMapping("/login")
 public class SessionController {
 	
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping("/login")
 	public String loginGET(Model model) {
 		model.addAttribute("user", new User());
 		model.addAttribute("userList", userService.findAll());
